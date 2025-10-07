bc802eef5897a6a4684c89fda5c3498624f8ebe8
karthik22102 <maheshwaramkarthik96@gmail.com>
2025-10-07 16:57:04 +0530

    for deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from bellman_ford import bellman_ford, reconstruct_path
+import os
 import json
 
 app = Flask(__name__)
@@ -50,4 +51,5 @@ def get_topology():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
